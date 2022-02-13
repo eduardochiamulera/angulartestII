968988fbdc1f6690f0d1c9932644bb735028066c
Eduardo Antonio Fraga Chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-02-13 00:32:22 -0300

    angular testes avançados

---
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -0,0 +1,49 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
+import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotoListModule } from './photo-list.module';
+
+describe(PhotoListComponent.name, () => {
+    let fixture: ComponentFixture<PhotoListComponent>
+    let component: PhotoListComponent;
+    let service: PhotoBoardService;
+
+    beforeEach(async() => {
+        await TestBed.configureTestingModule({
+            imports: [
+                PhotoListModule,
+                HttpClientModule
+            ],
+        }).compileComponents;
+        fixture = TestBed.createComponent(PhotoListComponent);
+        component = fixture.componentInstance;
+        service = TestBed.inject(PhotoBoardService);
+    });
+
+    it('should create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it(`(D) Should display board when data arrives`, () => {
+        const photos = buildPhotoList();
+        spyOn(service, 'getPhotos').and.returnValue(of(photos));
+        fixture.detectChanges();
+        const board = fixture.nativeElement.querySelector('app-photo-board')
+        const loader = fixture.nativeElement.querySelector('.loader');
+        expect(board).withContext('should display board').not.toBeNull();
+        expect(loader).withContext('should not display loader').toBeNull();
+    });
+
+    it(`(D) Should display loader while waiting for data`, () => {
+        spyOn(service, 'getPhotos').and.returnValue(null);
+        fixture.detectChanges();
+        const board = fixture.nativeElement.querySelector('app-photo-board')
+        const loader = fixture.nativeElement.querySelector('.loader');
+        expect(board).withContext('Should not display board').toBeNull();
+        expect(loader).withContext('should display loader').not.toBeNull();
+    });
+});
